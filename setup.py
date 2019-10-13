dd34b1250470a4d39010b47c03857461fb99f1d6
Niels Claes <niels.claes@kuleuven.be>
2019-10-13 20:06:19 +0200

    extended setup with required packages

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup, find_packages
 import os
 
 project_name = "amrvac_tools"
+required = ['numpy', 'scipy', 'matplotlib', 'vtk']
+version = '1.0.0'
 
 setup(
     name = project_name,
+    version = version,
     keywords = "interface data-analysis",
     python_requires = ">=3.6",
+    install_requires = required,
     packages = find_packages(),
 )
