d86370449e8a169d8ce289d86ca03b8a7df2a5d7
Niels Claes <niels.claes@kuleuven.be>
2019-10-13 20:06:04 +0200

    modified __inits__ for package preparation

--- amrvac_tools/vtkfiles/__init__.py
+++ amrvac_tools/vtkfiles/__init__.py
@@ -1,1 +1,4 @@
-from . import amrplot, numpy_support, read, streamplot
+from .amrplot import polyplot, rgplot, polyanim, line, plotoverline, velovect, contour, streamlines
+from . import numpy_support
+from .read import load_vtkfile
+from .streamplot import streamplot
